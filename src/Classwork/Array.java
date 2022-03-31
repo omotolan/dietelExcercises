fe220b3fe9c67e1c51d9f2efbfd98801b59aa1fa
Black_Tolani <akinsolatolani@yahoo.com>
2022-03-31 16:10:50 +0100

    added array practice

---
+++ src/Classwork/Array.java
@@ -0,0 +1,8 @@
+package Classwork;
+//practising array
+public class Array {
+    public static void main(String[] args) {
+        String[] boyNames = {"tola", "biodun", "tolu", "bassey"};
+        System.out.println(boyNames[3]);
+    }
+}
