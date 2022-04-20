78bd9a8911fcfc957a4aef6cff792bbfefefd491
Akinsola Tolani <akinsolatolani@yahoo.com>
2022-04-20 12:20:04 +0100

    Chapter8 Time

---
+++ src/Chapter6/Allowance.java
@@ -0,0 +1,35 @@
+package Chapter6;
+
+public class Allowance {
+    static private double salary;
+
+    public static void main(String[] args) {
+        salary = calculateTakeHomePay(1_000_000);
+        System.out.println(salary);
+
+    }
+
+    public static double calculateTakeHomePay(double basicSalary) {
+        double takeHomePay = basicSalary;
+        takeHomePay += calculateTransportAllowanceFrom(basicSalary);
+        takeHomePay += getFeedAllowanceFrom(basicSalary);
+        takeHomePay -= calculateTaxFrom(basicSalary);
+        return takeHomePay;
+
+    }
+
+    private static double calculateTransportAllowanceFrom(double basicSalary) {
+        final double TEN_PERCENT = 0.1;
+        return TEN_PERCENT * basicSalary;
+    }
+
+    private static double getFeedAllowanceFrom(double basicSalary) {
+        final double THREE_PERCENT = 0.03;
+        return THREE_PERCENT * basicSalary;
+    }
+
+    private static double calculateTaxFrom(double basicSalary) {
+        final double TWO_PERCENT = 0.02;
+        return TWO_PERCENT * basicSalary;
+    }
+}
--- src/Chapter8/Time.java
+++ src/Chapter8/Time.java
@@ -5,6 +5,14 @@ public class Time {
     private int minute;
     private int second;
 
+    public Time(int hour, int minute, int second) {
+        validate(hour, minute, second);
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+
+    }
+
     public void setTime(int hour, int minute, int second) {
         validate(hour, minute, second);
 
@@ -14,25 +22,60 @@ public class Time {
     }
 
     public void validate(int hour, int minute, int second) {
+        validateHour(hour);
+        validateMinute(minute);
+        validateSecond(second);
+    }
+
+    private void validateHour(int hour) {
         /*
         if (hour < 0 || hour > 24) {
             throw new IllegalArgumentException("Invalid hour");
         } */
-        //validate hour
         boolean hourIsValid = hour < 0 || hour > 24;
         if (hourIsValid) {
             throw new IllegalArgumentException("Invalid hour");
         }
-        // validate minute
+    }
+
+    private void validateMinute(int minute) {
         boolean minuteIsValid = minute < 0 || minute >= 60;
         if (minuteIsValid) {
             throw new IllegalArgumentException("Invalid minute");
         }
-        // validate second
+    }
+
+    private void validateSecond(int second) {
         boolean secondIsValid = second < 0 || second >= 60;
         if (secondIsValid) {
             throw new IllegalArgumentException("Invalid seconds");
         }
+    }
 
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        validateHour(hour);
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        validateMinute(minute);
+        this.minute = minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        validateSecond(second);
+        this.second = second;
     }
 }
