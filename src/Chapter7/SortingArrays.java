a1241ae9c3766f11f4a595d8a609ce9f955aa2a5
Akinsola Tolani <akinsolatolani@yahoo.com>
2022-04-21 14:47:36 +0100

    Updated chapter7

--- src/Chapter7/SortingArrays.java
+++ src/Chapter7/SortingArrays.java
@@ -1,25 +1,40 @@
 package Chapter7;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class SortingArrays {
     public static void main(String[] args) {
         int[] numbers = {45, 45, 2, 3, 6, 87, 6, 34, 23};
         Arrays.sort(numbers);
         System.out.println(Arrays.toString(numbers));
+        //called on method sort
+        sortNumbersInAscendingOrder(numbers);
 
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
-
-        }
         System.out.println();
 
-        // sort from one particular index to another
+        // called on method to sort from one particular index to another
         int[] figures = {45, 45, 2, 3, 6, 87, 6, 34, 23};
-        Arrays.sort(figures, 1, 5);
-        for (int i = 0; i < figures.length; i++) {
-            System.out.print(figures[i] + " ");
+        sortNumbersFromOneIndexToAnother(figures, 1, 5);
 
+    }
+    private static void sortNumbersInAscendingOrder(int[] array){
+        Arrays.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
     }
+    private static void sortNumbersFromOneIndexToAnother(int[] array, int fromIndex, int toIndex){
+        Arrays.sort(array, fromIndex, toIndex);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+    private static void sortNumbersInDescendingOrder(int[] array){
+        Arrays.sort(array, Collections.reverseOrder());
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
 }
