9156052f83124e9c174ef8a8007a0d43622e6abc
Akinsola Tolani <akinsolatolani@yahoo.com>
2022-04-29 16:06:34 +0100

    updated chapter 7

--- src/Chapter7/StudentPoll.java
+++ src/Chapter7/StudentPoll.java
@@ -2,10 +2,11 @@ package Chapter7;
 
 public class StudentPoll {
     public static void main(String[] args) {
-        int[] studentPoll = {1, 2, 4, 5, 2 , 5, 2, 1, 2, 2, 1, 4, 3, 5, 1, 3, 2, 4, 5, 1, 3, 3, 2, 4, 4, 4};
+        int[] studentPoll = {1, 2, 4, 5, 2, 5, 2, 1, 2, 2, 1, 4, 3, 5, 1, 3, 2, 4, 5, 1, 3, 3, 2, 4, 4, 4};
         int[] frequency = new int[6];
 
         for (int i = 0; i < studentPoll.length; i++) {
+            //frequency is taking and counting the values of studentPoll
             frequency[studentPoll[i]]++;
 
         }
