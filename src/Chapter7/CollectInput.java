e24ecbfaa5e9269266e35fd4556deb22060617b8
Akinsola Tolani <akinsolatolani@yahoo.com>
2022-04-30 23:54:12 +0100

    updated chapter 7

---
+++ src/Chapter7/CollectInput.java
@@ -0,0 +1,39 @@
+package Chapter7;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CollectInput {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        System.out.println("Enter length of array: ");
+        int number = input.nextInt();
+        int[] numbers = new int [number];
+        userInput(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+        System.out.println("Enter length of array: ");
+        int value = input.nextInt();
+       int[] values = userInput(value);
+        System.out.println(Arrays.toString(values));
+    }
+    public static int[] userInput(int number){
+        System.out.println("enter " + number + " numbers: ");
+        int[] values = new int[number];
+        for (int i = 0; i < values.length ; i++) {
+            values[i] = input.nextInt();
+
+        }
+        return values;
+    }
+    public static int[] userInput(int[] array){
+        System.out.println("enter  numbers: ");
+       // int[] values = new int[number];
+        for (int i = 0; i < array.length ; i++) {
+            array[i] = input.nextInt();
+
+        }
+        return array;
+    }
+}
