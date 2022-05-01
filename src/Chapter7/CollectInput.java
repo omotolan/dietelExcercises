0de96430ca4432be3c120259cfee49f5027665ef
Akinsola Tolani <akinsolatolani@yahoo.com>
2022-05-01 22:33:09 +0100

    updated chapter 7

--- src/Chapter7/CollectInput.java
+++ src/Chapter7/CollectInput.java
@@ -5,32 +5,35 @@ import java.util.Scanner;
 
 public class CollectInput {
     static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         System.out.println("Enter length of array: ");
         int number = input.nextInt();
-        int[] numbers = new int [number];
+        int[] numbers = new int[number];
         userInput(numbers);
         System.out.println(Arrays.toString(numbers));
 
         System.out.println("Enter length of array: ");
         int value = input.nextInt();
-       int[] values = userInput(value);
+        int[] values = userInput(value);
         System.out.println(Arrays.toString(values));
     }
-    public static int[] userInput(int number){
+
+    public static int[] userInput(int number) {
         System.out.println("enter " + number + " numbers: ");
         int[] values = new int[number];
-        for (int i = 0; i < values.length ; i++) {
+        for (int i = 0; i < values.length; i++) {
             values[i] = input.nextInt();
 
         }
         return values;
     }
-    public static int[] userInput(int[] array){
+
+    public static int[] userInput(int[] array) {
         System.out.println("enter  numbers: ");
-       // int[] values = new int[number];
-        for (int i = 0; i < array.length ; i++) {
+        // int[] values = new int[number];
+        for (int i = 0; i < array.length; i++) {
             array[i] = input.nextInt();
 
         }
--- src/Chapter7/SortArrayDescendingOrder.java
+++ src/Chapter7/SortArrayDescendingOrder.java
@@ -1,5 +1,43 @@
 package Chapter7;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class SortArrayDescendingOrder {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter length of array: ");
+        int number = input.nextInt();
+        int[] numbers = collectInput(number);
+        printArray(numbers);
+        System.out.println();
+        sortArrayInDescendingOrder(numbers);
+        printArray(numbers);
+
+    }
+    public static int[] collectInput(int number){
+        System.out.println("Enter " + number + " numbers: ");
+        int[] values = new int[number];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = input.nextInt();
+
+        }
+        return values;
+
+    }
+    public static void printArray(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+    }
+    public static void sortArrayInDescendingOrder(int [] array){
+        Arrays.sort(array);
+
+        for (int i = 0; i < array.length ; i++) {
+            
+        }
+
+    }
 
 }
