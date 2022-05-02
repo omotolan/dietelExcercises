30310e39e996e62a617df76346bf7cd136aacd9c
Akinsola Tolani <akinsolatolani@yahoo.com>
2022-05-02 22:11:29 +0100

    updated chapter 7

--- src/Chapter5/VaryingInterestRate.java
+++ src/Chapter5/VaryingInterestRate.java
@@ -8,10 +8,10 @@ public class VaryingInterestRate {
         for (rate = 5; rate <= 10; rate++) {
 
 
-            System.out.printf("%s     %s %n", "Year", "Amount on deposit" );
+            System.out.printf("%s     %s %n", "Year", "Amount on deposit");
             for (int year = 1; year <= 10; ++year) {
 
-                double amount = principal * Math.pow((1.0 + (rate*0.01)), year);
+                double amount = principal * Math.pow((1.0 + (rate * 0.01)), year);
 
                 System.out.printf("%4d%,20.2f%n", year, amount);
 
