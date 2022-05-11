8ec178ece805a1b1f91dc3e6fb431223d240536b
Akinsola Tolani <akinsolatolani@yahoo.com>
2022-05-11 17:47:25 +0100

    updated linkedlist and abstraction

---
+++ src/LinkedList/ArrayList.java
@@ -0,0 +1,33 @@
+package LinkedList;
+
+public class ArrayList implements List {
+    @Override
+    public void add(String item) {
+
+    }
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public String add(int index, String item) {
+        return null;
+    }
+
+    @Override
+    public void remove(String item) {
+
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
--- src/LinkedList/List.java
+++ src/LinkedList/List.java
@@ -2,8 +2,14 @@ package LinkedList;
 
 public interface List {
     void add(String item);
+
     String get(int index);
+
+    String add(int index, String item);
+
     void remove(String item);
+
     void remove(int index);
+
     int size();
 }
