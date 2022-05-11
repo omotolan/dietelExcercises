7333b3770c997063ecd0448d7716ad72f1cdc734
Akinsola Tolani <akinsolatolani@yahoo.com>
2022-05-11 18:02:03 +0100

    updated linkedlist and abstraction

--- src/OOP/PapaNosaDriver.java
+++ src/OOP/PapaNosaDriver.java
@@ -2,11 +2,14 @@ package OOP;
 
 public class PapaNosaDriver {
     public static void main(String[] args) {
-        Pray pray = new Pray2();
+        Pray pray = new Pray(); // super pray class
+        Pray prayy = new Pray1(); // sub pray class declared in the supper class type
+        Pray1 prayyy = new Pray1(); // sub pray class declared in the sub class type
         Walk walk = new Walk();
         PapaNosa nosa = new PapaNosa(walk, pray);
-
+        // here you can create various prayer types and pass to the objects
         nosa.pray();
         nosa.walk();
+        nosa.pray();
     }
 }
