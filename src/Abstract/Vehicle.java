0c99e09c3b284ac283e96db194fb9a7ca6896997
Akinsola Tolani <akinsolatolani@yahoo.com>
2022-05-11 17:11:44 +0100

    added linkedlist and abstraction

---
+++ src/Abstract/Vehicle.java
@@ -0,0 +1,8 @@
+package Abstract;
+
+public abstract class Vehicle {
+    public void move(){
+        System.out.println("move");
+    }
+    public abstract void turnRight();
+}
---
+++ src/LinkedList/List.java
@@ -0,0 +1,9 @@
+package LinkedList;
+
+public interface List {
+    void add(String item);
+    String get(int index);
+    void remove(String item);
+    void remove(int index);
+    int size();
+}
