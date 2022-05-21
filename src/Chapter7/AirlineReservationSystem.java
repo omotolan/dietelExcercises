f90a03cf8d4e8ede1e41a0170db633da0fcdad7d
Akinsola Tolani <akinsolatolani@yahoo.com>
2022-05-21 21:44:50 +0100

    updated airline reservation

--- src/Chapter7/AirlineReservationSystem.java
+++ src/Chapter7/AirlineReservationSystem.java
@@ -7,100 +7,95 @@ public class AirlineReservationSystem {
     private static boolean[] seats = new boolean[10];
 
 
+    private static int firstClassSeat;
+    private static int economyClassSeat = 6;
 
-    private static int firstClassSeat = 0;
-    private static int economyClassSeat = 5;
+    private static int select;
 
     static Scanner input = new Scanner(System.in);
+    private static boolean quit;
 
     public static void main(String[] args) {
-        //seats[0] = true;
 
-//        int quit = -1;
-//        for (int i = 0; i < seats.length; i++) {
-//            System.out.println("1 for first class, 2 for economy, -1 to quit: ");
-//            int option = input.nextInt();
-//            switch (option) {
-//                case 1 -> {
-//                    seats[i] = true;
-//                    if (seats[i] && i < 5) {
-//                        firstClassSeat++;
-//                    }
-//                }
-//                case 2 -> {
-//                    seats[i] = true;
-//                    if (seats[i] && i > 5) {
-//                        //firstClassSeat++;
-//                        economyClassSeat++;
-//                    }
-//                }
-//                default -> System.out.println("invalid selection");
-//            }
-//
-//        }
-        allocateClassSeat();
-        //set();
-       // setEconomyClassSeat();
+        option();
         System.out.println(Arrays.toString(seats));
         System.out.println(firstClassSeat);
         System.out.println(economyClassSeat);
 
     }
 
-    public static void allocateClassSeat() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("enter option: ");
-            int option = input.nextInt();
-            if (option == 1 && i < 5) {
-                seats[i] = true;
-                firstClassSeat++;
-                if (firstClassSeat == 5){
-                    System.out.println("first class full");
-                }
-            }  if (option == 2 && 9 > i) {
-                seats[i] = true;
-                economyClassSeat++;
-                if (economyClassSeat == 10){
-                    System.out.println("economy class full");
-                }
-                //} else {setEconomyClassSeat();}
-            }
-        }
-    }
 
     public static void setFirstClassSeat() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("enter option: ");
-            int option = input.nextInt();
-            if (option == 1 && i < 5) {
-                seats[i] = true;
-                firstClassSeat++;
-                if (firstClassSeat == 5) {
-                    System.out.println("first class full");
-setEconomyClassSeat();
-                }
-            }
+        if (firstClassSeat >= 0 && firstClassSeat < 6) {
+            seats[firstClassSeat] = true;
+referToEconomy();
+            firstClassSeat++;
         }
     }
-    public static void set(){
-        setFirstClassSeat();
-        setEconomyClassSeat();
+    public static void referToEconomy(){
+        if (firstClassSeat == 5) {
+            System.out.println("We are sorry to inform you our first class seats are completely booked");
+            System.out.println("But we do have economy available. Would you like to book economy?");
+            System.out.println("Type yes to proceed and no to cancel: ");
+            String answer = input.nextLine();
+            input.nextLine();
+            if (answer.equalsIgnoreCase("yes")) {
+                System.out.println("enter 2 to book economy");
+            select = input.nextInt();
+            setEconomyClassSeat();
+            } else {
+                System.exit(7);
+                System.out.println("Thank you for choosing Edo Airways!!!");
+            }
+        }
+//        if (firstClassSeat == 5){
+//            System.out.println("we only have economy class available");
+//            System.out.println("enter 2 to book economy");
+//            select = input.nextInt();
+//        }
     }
-        public static void setEconomyClassSeat () {
-            for (int i = economyClassSeat; i < 10; i++) {
-                System.out.println("enter option: ");
-                int option = input.nextInt();
-                 if (option == 2 && 9 > i) {
-                    seats[i] = true;
-                    economyClassSeat++;
-                    if (economyClassSeat == 10){
-                        System.out.println("economy class full");
-                        setFirstClassSeat();
-                    }
-                    //} else {setEconomyClassSeat();}
+
+
+    public static void setEconomyClassSeat() {
+
+        if (economyClassSeat < 11) {
+            seats[economyClassSeat] = true;
+            economyClassSeat++;
+
+            if (economyClassSeat == 10) {
+                System.out.println("economy class full");
+                System.out.println("We are sorry to inform you our economy class seats are completely booked");
+                System.out.println("But we do have first class seats available. Would you like to book economy?");
+                System.out.println("Type yes to book first class and no to cancel: ");
+                String answer = input.nextLine();
+                if (answer.equalsIgnoreCase("yes")) {
+                    setFirstClassSeat();
+                } else {
+                    System.exit(7);
+                    System.out.println("Thank you for choosing Edo Airways!!!");
                 }
             }
+
         }
 
+    }
+
+    public static void option() {
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("enter option: ");
+            select = input.nextInt();
+            input.nextLine();
+            switch (select) {
+                case 1 -> {setFirstClassSeat();
+                    System.out.println("You've been allotted seat number " + firstClassSeat);
+                }
+                case 2 -> {setEconomyClassSeat();System.out.println("You've been allotted seat number " + economyClassSeat);}
+                case 3 -> quit = true;
+            }
+        }
+    }
 
 }
+
+
