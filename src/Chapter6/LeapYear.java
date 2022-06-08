422774171cb7e2ae12e9caebff3e35f659815c8d
Akinsola Tolani <akinsolatolani@yahoo.com>
2022-06-08 09:28:05 +0100

    updated chapter6

---
+++ src/Chapter6/LeapYear.java
@@ -0,0 +1,33 @@
+package Chapter6;
+
+import java.time.YearMonth;
+import java.util.Scanner;
+
+public class LeapYear {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter year: ");
+        int year = input.nextInt();
+
+        yearType(year);
+
+
+    }
+
+    private static boolean isLeapYear(int year) {
+        int month = 2;
+        YearMonth yearOfTheMonth = YearMonth.of(year, month);
+        int daysInMonth = yearOfTheMonth.lengthOfMonth();
+        return daysInMonth > 28;
+    }
+
+    public static void yearType(int year) {
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
